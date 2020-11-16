d53d01581287c0f5beff74967372a53e14409575
ParkHyeonJae <md101106@gmail.com>
2020-11-17 08:46:36 +0900

    Add Boss Script

--- Assets/Scripts/Boss/Boss.cs
+++ Assets/Scripts/Boss/Boss.cs
@@ -9,12 +9,9 @@ public abstract class Boss : MonoBehaviour
 
     [Range(5f, 20f)]
     [SerializeField] float m_fResetTime = 10f;
-
-    [SerializeField] UnityEngine.UI.Image m_phaseTimeImage = null;
     private Timer m_timer;
     private void OnEnable()
     {
-        Debug.Assert(m_phaseTimeImage != null, "NullReference");
 
         m_timer = new Timer(m_fResetTime, true);
 
@@ -24,18 +21,9 @@ public abstract class Boss : MonoBehaviour
     {
         while(gameObject.activeInHierarchy)
         {
-            //int curTime = (int)m_timer.Operate();
-            //Debug.Log(curTime);
-            //m_phaseTimeImage.fillAmount = 1.0f / curTime;
-
-
-            if (m_timer.LimitOperate())
-            {
-
-            }
-            
 
             yield return null;
         }
     }
+
 }
